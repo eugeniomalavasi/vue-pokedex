83812ec0ae582770b7c6fd214fda4b6e2105e672
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-09-13 07:42:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SearchBar from './components/SearchBar.vue'
+</script>
+
+<template>
+  <SearchBar />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        <label for="searchInp">Pokemon Name</label>
+        <input type="text" id="searchInp" v-model="pokemonInp">
+        <button type="submit" class="btn btn-outline-primary">search</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
